cebaa07c1c416ad28141fd6336c5e81ea32a5d92
satyam56-svg <satyamkumar2351056@gmail.com>
2026-08-08 03:21:29 +0530

    Fix CORS for Vercel frontend

--- src/main/java/com/satyam/offline_upi_payment/config/SecurityConfig.java
+++ src/main/java/com/satyam/offline_upi_payment/config/SecurityConfig.java
@@ -39,16 +39,24 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
+
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://offline-upi-payment-frontend.vercel.app"
+                )
         );
+
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
         );
+
         configuration.setAllowedHeaders(
                 List.of("*")
         );
+
         configuration.setAllowCredentials(true);
+
         configuration.setExposedHeaders(
                 List.of("Authorization")
         );
